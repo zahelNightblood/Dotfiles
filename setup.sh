6cafc266e19a92d830c71bd45f0cd699dc9ca767
Matthais Pali <matthaispali@gmail.com>
2025-05-29 08:09:25 -0600

    added hyprland config to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 
 if [[ $1 == "desktop" ]]; then
-  rm -r ~.config/nvim
+  rm -r ~/.config/nvim
   ln -sv $(pwd)/nvimMain ~/.config/nvim
   if [[ $2 == "wm" ]]; then
-     pass
-  fi
-elif [[ $1 == "termux" ]]; then
-  pass
-fi
+    rm -r ~/.config/hypr
+    ln -sv $(pwd)/hypr ~/.config/hypr
