781116b98d5a8cf33c93463c4d08628270a3188c
Matthais Pali <matthaispali@gmail.com>
2025-07-05 09:22:39 -0600

    Minor Fixes

--- nvimMain/lua/plugins/lsp-config.lua
+++ nvimMain/lua/plugins/lsp-config.lua
@@ -15,28 +15,14 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+      lspconfig.lua_ls.setup({})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
+    end
   },
 }
