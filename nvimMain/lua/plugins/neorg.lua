fdaa329be7dd8e113fb21e9df9f256f38b4707ea
Matthais Pali <matthaispali@gmail.com>
2025-09-09 08:51:14 -0600

    readded neorg.lua

---
+++ nvimMain/lua/plugins/neorg.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-neorg/neorg",
+  lazy = false,
+  dependencies = { "nvim-lua/plenary.nvim" },
+  ft = "norg",
+  opts = {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.keybinds"] = {}, -- Adds default keybindings
+      ["core.journal"] = {}, -- Enables support for the journal module
+      ["core.dirman"] = { -- Manages Neorg workspaces
+        config = {
+          workspaces = {
+            notes = "~/projects/notes",
+          },
+        },
+      },
+    },
+  },
+}
